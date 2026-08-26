9b30d05562f2cf2e0590578c9a882a394f0f758c
Yernar <mr.asirov87@gmail.com>
2026-08-26 18:06:11 +0500

    Update calisthenics_bot.py

--- calisthenics_bot.py
+++ calisthenics_bot.py
@@ -11,9 +11,7 @@ def init_connection():
     url = st.secrets["SUPABASE_URL"]
     key = st.secrets["SUPABASE_KEY"]
     return create_client(url, key)
-
 supabase = init_connection()
-
 
 # Title
 st.title("Progress Tracker")
@@ -52,6 +50,9 @@ if st.button("Log Workout"):
         rep_list = [int(x.strip()) for x in reps_text.split(',')]
         total_reps = sum(rep_list)
         density = total_reps / duration
+        min_value = min(rep_list)
+        max_value = max(rep_list)
+        sets = len(rep_list)
 
         # Insert the data into Supabase
         supabase.table("workouts").insert({
@@ -60,7 +61,10 @@ if st.button("Log Workout"):
             "duration": duration,
             "total_reps": total_reps,
             "density": density,
-            "reps_array": reps_text
+            "reps_array": reps_text,
+            "min": min_value,
+            "max": max_value,
+            "sets": sets
         }).execute()
 
         st.success(f"Saved {total_reps} reps of {exercise_type} to Supabase!")
@@ -70,21 +74,49 @@ if st.button("Log Workout"):
 
 # Displays the database
 st.divider()
-st.subheader("Workout History")
+st.title("Workout History")
 
 # Read from Supabase (fetching all rows and ordering by date)
 response = supabase.table("workouts").select("*").order("date", desc=True).execute()
 df = pd.DataFrame(response.data)
-
-# Is my total reps growing per exercise?
-st.subheader("Is my total reps growing?")
-df = df.sort_values('date', ascending=True)
-fig, ax = plt.subplots()
-ax.plot(df['date'], df['total_reps'], marker='o')
-st.pyplot(fig)
 
 # Hide the database ID column for a cleaner UI
 if not df.empty:
     st.dataframe(df.drop(columns=['id']), use_container_width=True)
 else:
     st.info("No workouts logged yet.")
+
+
+# SUMMARY STATISTICS
+st.divider()
+st.title("Summary Statistics")
+
+# Button for showing which exercise to choose
+exercise_statistics = st.selectbox("Choose the exercise to show statistics",
+                             ["Handstand Push-ups",
+                              "Pull-ups",
+                              "Muscle-ups",
+                              "Dips"], key=1)
+
+# Quick Summary
+st.subheader(f"Quick Summary for {exercise_statistics}")
+minimum = df['min'].min()
+maximum = df['max'].max()
+average = sum(df['total_reps']) / sum(df['sets'])
+st.markdown(f"Minimum: {minimum}")
+st.markdown(f"Maximum: {maximum}")
+st.markdown(f"Average: {average}")
+
+
+# Is my density growing for the exercise?
+st.subheader(f"Is my endurance growing for {exercise_statistics}?")
+st.write("Endurance = Total reps / Duration")
+
+try:
+    df = df.sort_values('date', ascending=True)
+    fig, ax = plt.subplots()
+    ax.plot(df['date'], df.loc[df['exercise'] == exercise_statistics, 'density'], marker='o')
+    st.pyplot(fig)
+except Exception as e:
+    st.error("No data to process.")
+
