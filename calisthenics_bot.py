347be192c5265d0ef73bee31a002bd697bcdb5c7
Yernar <mr.asirov87@gmail.com>
2026-08-26 18:51:19 +0500

    Update calisthenics_bot.py

--- calisthenics_bot.py
+++ calisthenics_bot.py
@@ -99,15 +99,18 @@ exercise_statistics = st.selectbox("Choose the exercise to show statistics",
                               "Dips"], key=1)
 
 # Quick Summary
-st.divider()
-st.title(f"Quick Summary for {exercise_statistics}")
-minimum = df.loc[df['exercise'] == exercise_statistics, 'min'].min()
-maximum = df.loc[df['exercise'] == exercise_statistics, 'max'].max()
-average = sum(df.loc[df['exercise'] == exercise_statistics, 'total_reps']) / sum(df.loc[df['exercise'] == exercise_statistics, 'sets'])
+try:
+    st.divider()
+    st.title(f"Quick Summary for {exercise_statistics}")
+    minimum = df.loc[df['exercise'] == exercise_statistics, 'min'].min()
+    maximum = df.loc[df['exercise'] == exercise_statistics, 'max'].max()
+    average = sum(df.loc[df['exercise'] == exercise_statistics, 'total_reps']) / sum(df.loc[df['exercise'] == exercise_statistics, 'sets'])
 
-st.markdown(f"Minimum: {minimum}")
-st.markdown(f"Maximum: {maximum}")
-st.markdown(f"Average: {average}")
+    st.markdown(f"Minimum: {minimum}")
+    st.markdown(f"Maximum: {maximum}")
+    st.markdown(f"Average: {average}")
+except Exception as e:
+    st.error(f"No data to process. Error: {e}")
 
 # Is my endurance growing for the exercise?
 st.subheader(f"Is my endurance growing for {exercise_statistics}?")
